4f83048108c389cc1d925a2383e87c31a6655ea4
Avvy <saberwatch@gmail.com>
2022-02-15 16:27:09 -0800

    formatting

---
+++ js/theme.js
@@ -0,0 +1,27 @@
+function themeChange() {
+  const body = document.body;
+  const button = document.getElementById("theme");
+  if (localStorage.getItem("darkmode") === "true") {
+    body.classList.remove("darkmode");
+    localStorage.setItem("darkmode", false);
+    button.innerHTML = "dark-mode";
+    document.getElementById("theme-color").content = "#FFFFFF";
+  } else {
+    body.classList.add("darkmode");
+    localStorage.setItem("darkmode", true);
+    button.innerHTML = "light-mode";
+    document.getElementById("theme-color").content = "#000000";
+  }
+}
+
+function onLoad() {
+  const body = document.body;
+  const button = document.getElementById("theme");
+  if (localStorage.getItem("darkmode") === "true") {
+    body.classList.add("darkmode");
+    button.innerHTML = "light-mode";
+    document.getElementById("theme-color").content = "#000000";
+  }
+}
+
+onLoad();

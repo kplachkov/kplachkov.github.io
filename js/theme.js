ba1ae97ca91f7d0f6920bc0289d43277fb8d4705
Avvy <saberwatch@gmail.com>
2022-02-15 16:27:09 -0800

    Implement dark and light theme

---
+++ js/theme.js
@@ -0,0 +1,37 @@
+const darkModeStorageKey = "dark-mode";
+const rootDarkThemeClass = "dark-theme";
+
+function switchTheme() {
+	const buttonThemeSwitcher = document.getElementById("button-theme-switcher");
+	const themeColor = document.getElementById("theme-color");
+
+	if (localStorage.getItem(darkModeStorageKey) === "true") {
+		document.querySelector(":root").classList.remove(rootDarkThemeClass);
+
+		buttonThemeSwitcher.innerHTML = "dark-mode";
+		themeColor.content = "#F9F9F9";
+
+		localStorage.setItem(darkModeStorageKey, "false");
+	} else {
+		document.querySelector(":root").classList.add(rootDarkThemeClass);
+
+		buttonThemeSwitcher.innerHTML = "light-mode";
+		themeColor.content = "#282828";
+
+		localStorage.setItem(darkModeStorageKey, "true");
+	}
+}
+
+function onLoad() {
+	const buttonThemeSwitcher = document.getElementById("button-theme-switcher");
+	const themeColor = document.getElementById("theme-color");
+
+	if (localStorage.getItem(darkModeStorageKey) === "true") {
+		document.querySelector(":root").classList.add(rootDarkThemeClass);
+
+		buttonThemeSwitcher.innerHTML = "light-mode";
+		themeColor.content = "#282828";
+	}
+}
+
+onLoad();
